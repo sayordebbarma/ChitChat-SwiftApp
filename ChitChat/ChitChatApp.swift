ccdd42ecd9a972da54a1ebd798e7003581d27ad2
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-03-10 20:23:51 +0530

    Initial Commit

---
+++ ChitChat/ChitChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChitChatApp.swift
+//  ChitChat
+//
+//  Created by Sayor Debbarma on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChitChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
